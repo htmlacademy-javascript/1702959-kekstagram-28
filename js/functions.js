d0715d3f7deeb9838107c7f593e00ae017fc2592
mvn-h24 <morning.vecher.night@yandex.ru>
2023-02-19 11:49:46 +0300

    feat: Добавляет numberExtract

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,15 @@ function isPalindrome(expectation){
   const src = expectation.toString().toLowerCase().replaceAll(' ', '');
   return src === src.split('').reverse().join('');
 }
+
+function numberExtract(expectation){
+  const src = expectation.toString();
+  let result = '';
+  for(const item of src){
+    const parsed = parseInt(item, 10);
+    if(!isNaN(parsed)){
+      result += parsed;
+    }
+  }
+  return result.length !== 0 ? parseInt(result, 10) : NaN;
+}
--- test/funcitons.test.js
+++ test/funcitons.test.js
@@ -1,5 +1,5 @@
 describe('isLessThan', () => {
-  it('academy expections', () => {
+  it('academy expectations', () => {
     expect(isLessThan('проверяемая строка', 20)).toBeTruthy();
     expect(isLessThan('проверяемая строка', 18)).toBeTruthy();
     expect(isLessThan('проверяемая строка', 10)).toBeFalsy();
@@ -8,10 +8,23 @@ describe('isLessThan', () => {
 
 
 describe('isPalindrome', () => {
-  it('academy expections', () => {
+  it('academy expectations', () => {
     expect(isPalindrome('топот')).toBeTruthy();
     expect(isPalindrome('ДовОд')).toBeTruthy();
     expect(isPalindrome('Кекс')).toBeFalsy();
     expect(isPalindrome('Лёша на полке клопа нашёл ')).toBeTruthy();
+  })
+});
+
+
+describe('numberExtract', () => {
+  it('academy expectations', () => {
+    expect(numberExtract('2023 год')).toBe(2023);
+    expect(numberExtract('ECMAScript 2022')).toBe(2022);
+    expect(numberExtract('1 кефир, 0.5 батона')).toBe(105);
+    expect(numberExtract('а я томат')).toBeNaN();
+    expect(numberExtract(2023)).toBe(2023);
+    expect(numberExtract(-1)).toBe(1);
+    expect(numberExtract(1.5)).toBe(15);
   })
 });
