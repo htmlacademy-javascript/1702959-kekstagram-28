adead5e8070e69d202d3e1fd3c839850eb76766f
mvn-h24 <morning.vecher.night@yandex.ru>
2023-04-01 08:29:13 +0300

    feat: настраивает странное поведение

--- js/entity/comment/ui/photo-comment-list.js
+++ js/entity/comment/ui/photo-comment-list.js
@@ -1,20 +1,25 @@
-import { createPhotoCommentList } from './photo-comment.js';
-const getCounterBody = (showedCount, commentCount) => `${showedCount} из <span class="comments-count">${commentCount}</span> комментариев`
+import { createPhotoComment } from './photo-comment.js';
+const getCounterBody = (showedCount, commentCount) => `${showedCount} из <span class="comments-count">${commentCount}</span> комментариев`;
 
 export const photoCommentList = (comments) => {
   const PAGE_SIZE = 5;
   let page = 0;
   const pageCount = Math.ceil(comments.length / PAGE_SIZE);
   const hiddenClass = 'hidden';
-
+  const domPhotoList = comments.map((comment) => createPhotoComment(comment));
   const render = (commentList, commentCounter, commentLoader) => {
     const loadComments = () => {
       if (page < pageCount) {
         page++;
-        const commentSlice = comments.slice(0, page * PAGE_SIZE);
+        const commentSlice = domPhotoList.slice(0, page * PAGE_SIZE);
         commentList.innerHTML = '';
-        commentList.append(createPhotoCommentList(commentSlice));
-        commentCounter.innerHTML = getCounterBody(commentSlice.length, comments.length);
+        commentList.append(
+          commentSlice.reduce(
+            (result, comment) => (result.append(comment), result),
+            document.createDocumentFragment()
+          )
+        );
+        commentCounter.innerHTML = getCounterBody(commentSlice.length, domPhotoList.length);
       }
       if (page >= pageCount) {
         commentLoader.classList.add(hiddenClass);
