3e98ba5dedd5faa45aeb58c34d8930405f3e3799
mvn-h24 <morning.vecher.night@yandex.ru>
2023-04-06 01:29:43 +0300

    feat: настраивает фильтры

--- js/entity/photo/ui/scale-controller.js
+++ js/entity/photo/ui/scale-controller.js
@@ -1,6 +1,6 @@
 import { useCallback } from '../../../shared/useCallback.js';
 
-const createRangeController = ({ initialValue, step, range } = { initialValue: 55, step: 5, range: { min: 0, max: 100 } }) => {
+const createRangeController = ({ initialValue, step, range } = { initialValue: 100, step: 25, range: { min: 25, max: 100 } }) => {
   const moreKeyClass = 'scale__control--bigger';
   const lessKeyClass = 'scale__control--smaller';
   const scaleKeys = document.querySelectorAll('button.scale__control');
@@ -18,29 +18,30 @@ const createRangeController = ({ initialValue, step, range } = { initialValue: 5
       onUpdate.call(value);
     });
   });
+  const reset = () => {
+    value = initialValue;
+    onUpdate.call(value);
+  };
 
   return {
     onUpdate: onUpdate.set,
     getValue: () => value,
+    reset,
   };
 };
 
-const createScaleController = () => {
+const createScaleController = (scaleImg) => {
   const inputValue = document.querySelector('.scale__control.scale__control--value');
-  const setInputValue = (v) => {
-    inputValue.value = `${v}%`;
+  const setupValue = (value) => {
+    inputValue.value = `${value}%`;
+    scaleImg.style.transform = `scale(${value * 0.01})`;
   };
-  const imageScale = document.querySelector('.img-upload__preview');
-  const setPhotoScale = (v) => {
-    imageScale.style.transform = `scale(0.${v})`;
-  };
+
   const rangeController = createRangeController();
-
-  rangeController.onUpdate((value) => {
-    setInputValue(value);
-    setPhotoScale(value);
-  });
-  setPhotoScale(rangeController.getValue());
-
+  rangeController.onUpdate(setupValue);
+  setupValue(rangeController.getValue());
+  return {
+    reset: rangeController.reset
+  };
 };
 export { createScaleController };
--- js/widget/photo-upload-form.js
+++ js/widget/photo-upload-form.js
@@ -1,19 +1,21 @@
 import { createPhotoUploader } from '../entity/photo/ui/uploader.js';
 import { createPhotoEditDialog } from '../entity/photo/ui/photo-edit-dialog/index.js';
 import { createEffectSlider } from '../entity/photo/ui/photo-effect-slider.js';
-import {createScaleController}from '../entity/photo/ui/scale-controller.js';
+import { createScaleController } from '../entity/photo/ui/scale-controller.js';
 
 export const createPhotoUploadForm = () => {
-  createScaleController();
-  createEffectSlider();
+  const scaleControl = createScaleController(document.querySelector('.img-upload__preview img'));
+  const effectSlider = createEffectSlider();
   const dialog = createPhotoEditDialog();
   const fileUloader = createPhotoUploader();
 
   fileUloader.onUpload((file) => {
+    scaleControl.reset();
     dialog.setFile(file);
     dialog.show();
   });
   dialog.onClose(() => {
+    effectSlider.reset();
     fileUloader.cleanup();
   });
 };
