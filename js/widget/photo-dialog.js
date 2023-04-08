b468ba84bef5e73e6d1cb24474efebd55856ae6a
mvn-h24 <morning.vecher.night@yandex.ru>
2023-04-08 13:09:55 +0300

    fix: naming

--- js/widget/photo-dialog.js
+++ js/widget/photo-dialog.js
@@ -3,12 +3,12 @@ import { usePhoto } from '../entity/photo/store/usePhoto.js';
 import { createPhotoCommentList } from '../entity/comment/photo-comment-list.js';
 
 export const createPhotoDialog = () => {
-  const kekstagramStore = usePhoto();
+  const photoStore = usePhoto();
 
   document.querySelectorAll('.picture').forEach((photo) => {
     photo.addEventListener('click', (event) => {
       event.preventDefault();
-      const photoData = kekstagramStore.getPhotoById(photo.dataset.id);
+      const photoData = photoStore.getPhotoById(photo.dataset.id);
       const commentList = createPhotoCommentList({ commentListData: photoData.comments });
       createPhotoDetail(photoData).render({ commentList });
     });
